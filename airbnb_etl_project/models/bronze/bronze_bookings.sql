664ebdcc671935cc9cd46f7ba1d92c92da9488b8
Abderrahmen-Malouche <121557323+Abderrahmen-Malouche@users.noreply.github.com>
2026-02-14 22:26:26 +0100

    finished the bronze layer : validating and casting the columns

--- airbnb_etl_project/models/bronze/bronze_bookings.sql
+++ airbnb_etl_project/models/bronze/bronze_bookings.sql
@@ -1,1 +1,15 @@
-select * from {{source("staging","bookings")}}
+select 
+    trim(booking_id)                    AS booking_id,
+    listing_id::int                     AS listing_id,
+    booking_date::date                  AS booking_date,
+    nights_booked::int                  AS nights_booked,
+    booking_amount::numeric             AS booking_amount,
+    cleaning_fee::numeric               AS cleaning_fee,
+    service_fee::numeric                AS service_fee,
+    CASE 
+        WHEN lower(trim(booking_status)) IN ('confirmed', 'cancelled') THEN lower(trim(booking_status))
+        ELSE NULL
+    END                                 AS booking_status,
+    created_at::timestamp               AS created_at
+FROM 
+    {{source("staging","bookings")}}
--- airbnb_etl_project/models/bronze/bronze_hosts.sql
+++ airbnb_etl_project/models/bronze/bronze_hosts.sql
@@ -1,1 +1,13 @@
-select * from {{source("staging","hosts")}}
+
+select 
+    host_id::int             AS host_id,
+    trim(host_name)          AS host_name,
+    host_since::date         AS host_since,
+    is_superhost::boolean    AS is_superhost,
+    CASE 
+        WHEN response_rate between 0 AND 100 THEN response_rate
+        ELSE NULL
+    END AS response_rate,
+    created_at::timestamp    AS created_at
+FROM {{source("staging","hosts")}}
+
